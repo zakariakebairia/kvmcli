b91ad1a69c08518e8c8444b3fe5b5fdc9f93434f
Zakaria Kebairia <4.kebairia@gmail.com>
2026-04-19 00:09:06 +0100

    fix(network): delete manager.go file
    - Delete manager.go file  and move its functions to dhcp.go

--- internal/providers/network/dhcp.go
+++ internal/providers/network/dhcp.go
@@ -71,3 +71,33 @@ func dhcpHostXML(mac net.HardwareAddr, ip net.IP) string {
 func dhcpHostSelectorXML(mac net.HardwareAddr) string {
 	return fmt.Sprintf(`<host mac='%s'/>`, mac.String())
 }
+
+// TODO: I need to fix this, not clean, over engineered
+// SetStaticMapping ensures a DHCP reservation (MAC → IP) exists on a libvirt network.
+func SetStaticMapping(session registry.Session, spec *registry.Object, hostAddr *HostAddr) error {
+	networkName := spec.GetString("network")
+
+	flags := libvirt.NetworkUpdateAffectLive | libvirt.NetworkUpdateAffectConfig
+
+	nw, err := session.Conn.NetworkLookupByName(networkName)
+	if err != nil {
+		return fmt.Errorf("lookup network %q: %w", networkName, err)
+	}
+	if err := modifyDHCPHost(session, nw, hostAddr.IP, hostAddr.MAC, flags); err == nil {
+		return nil
+	}
+
+	_ = deleteDHCPHost(session, nw, hostAddr.MAC, flags)
+
+	if err := addDHCPHost(session, nw, hostAddr.IP, hostAddr.MAC, flags); err != nil {
+		return fmt.Errorf(
+			"set dhcp mapping on network %q (mac=%s ip=%s): %w",
+			networkName,
+			hostAddr.MAC,
+			hostAddr.IP,
+			err,
+		)
+	}
+
+	return nil
+}
